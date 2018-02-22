bf4e3efc37d41a46c5722b7e5315b74f74d7f1ec
AndrewMik <Andrew.Mikitchuk@gmail.com>
2018-02-23 01:15:48 +0300

    Task_LoveTriangles: Solution of the task 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let loveTrianglesCounter = 0;
+
+  preferences.forEach(function(firstPersonPreference, i, preferences){
+    
+    if (i < 0) return;
+    
+    firstPerson = i;
+    secondPerson = firstPersonPreference - 1;
+    thirdPerson = preferences[secondPerson] - 1;
+    thirdPersonPreference = preferences[thirdPerson] - 1;
+
+    if (preferences[firstPerson] == preferences[secondPerson] || 
+        preferences[secondPerson] == preferences[thirdPerson] || 
+        preferences[thirdPerson] == preferences[firstPerson]) 
+        return;
+
+    if (firstPerson == thirdPersonPreference){
+      loveTrianglesCounter++;
+      preferences[firstPerson] = preferences[secondPerson] = preferences[thirdPerson] = -1;
+    } else {
+      preferences[firstPerson] = -1;
+    }
+
+  });
+
+return loveTrianglesCounter;
+
 };
